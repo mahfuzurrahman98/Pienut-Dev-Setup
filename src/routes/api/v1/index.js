c3e15576710dabe473a0fb75fbd7d402b2e7b05d
mahfuzurrahman98 <mdmahfuzurrahmanarif@gmail.com>
2023-03-21 22:22:47 +0600

    user route fixed

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,12 +1,11 @@
 import { Route } from 'pienut';
 import authRoutes from './authRoutes.js';
+import userRoutes from './userRoutes.js';
 
 // auth routes
 Route.use('/auth', authRoutes);
 
 // user routes
 Route.use('/users', userRoutes);
-Route.get('/users/:id', UserController, '_show');
-Route.delete('/users/:id', UserController, '_destroy');
 
 export default Route.router;
--- src/routes/api/v1/userRoutes.js
+++ src/routes/api/v1/userRoutes.js
@@ -2,8 +2,8 @@ import { Auth, Route } from 'pienut';
 import UserController from '../../../controllers/UserController.js';
 
 Route.get('/', UserController, '_index', Auth.isAuthenticated);
-Route.get('/:id', UserController, 'show');
-Route.delete('/:id', UserController, 'delete');
-Route.put('/:id', UserController, 'update');
+Route.get('/:id', UserController, '_show');
+Route.delete('/:id', UserController, '_destroy');
+Route.put('/:id', UserController, '_update');
 
 export default Route.router;
